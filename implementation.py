47a43da0762e3dc08c0c6f7c9e24d4f836c4f2a7
Kaleado <kaleadoskope@hotmail.com>
2018-09-23 17:07:51 +1000

    Added other things.

--- implementation.py
+++ implementation.py
@@ -1,27 +1,65 @@
 import tensorflow as tf
 import re
 
-BATCH_SIZE = 128
+BATCH_SIZE = 512
 MAX_WORDS_IN_REVIEW = 100  # Maximum length of a review to consider
 EMBEDDING_SIZE = 50  # Dimensions for each word vector
 
-stop_words = set({'ourselves', 'hers', 'between', 'yourself', 'again',
-                  'there', 'about', 'once', 'during', 'out', 'very', 'having',
-                  'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its',
-                  'yours', 'such', 'into', 'of', 'most', 'itself', 'other',
-                  'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him',
-                  'each', 'the', 'themselves', 'below', 'are', 'we',
-                  'these', 'your', 'his', 'through', 'don', 'me', 'were',
-                  'her', 'more', 'himself', 'this', 'down', 'should', 'our',
-                  'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had',
-                  'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them',
-                  'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does',
-                  'yourselves', 'then', 'that', 'because', 'what', 'over',
-                  'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you',
-                  'herself', 'has', 'just', 'where', 'too', 'only', 'myself',
-                  'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being',
-                  'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it',
-                  'how', 'further', 'was', 'here', 'than'})
+stop_words = set({'a', 'about', 'above', 'across', 'after', 'again', 'against',
+'all', 'almost', 'alone', 'along', 'already', 'also', 'although',
+'always', 'among', 'an', 'and', 'another', 'any', 'anybody',
+'anyone', 'anything', 'anywhere', 'are', 'area', 'areas', 'around',
+'as', 'ask', 'asked', 'asking', 'asks', 'at', 'away', 'b', 'back',
+'backed', 'backing', 'backs', 'be', 'became', 'because', 'become',
+'becomes', 'been', 'before', 'began', 'behind', 'being', 'beings',
+'best', 'better', 'between', 'big', 'both', 'but', 'by', 'c', 'came',
+'can', 'cannot', 'case', 'cases', 'certain', 'certainly', 'clear',
+'clearly', 'come', 'could', 'd', 'did', 'differ', 'different', 'differently',
+'do', 'does', 'done', 'down', 'downed', 'downing', 'downs', 'during', 'e',
+'each', 'early', 'either', 'end', 'ended', 'ending', 'ends', 'enough',
+'even', 'evenly', 'ever', 'every', 'everybody', 'everyone', 'everything',
+'everywhere', 'f', 'face', 'faces', 'fact', 'facts', 'far', 'felt',
+'few', 'find', 'finds', 'first', 'for', 'four', 'from', 'full', 'fully',
+'further', 'furthered', 'furthering', 'furthers', 'g', 'gave', 'general',
+'generally', 'get', 'gets', 'give', 'given', 'gives', 'go', 'going',
+'good', 'goods', 'got', 'great', 'greater', 'greatest', 'group',
+'grouped', 'grouping', 'groups', 'h', 'had', 'has', 'have',
+'having', 'he', 'her', 'here', 'herself', 'high', 'higher',
+'highest', 'him', 'himself', 'his', 'how', 'however', 'i',
+'if', 'important', 'in', 'interest', 'interested', 'interesting',
+'interests', 'into', 'is', 'it', 'its', 'itself', 'j', 'just',
+'k', 'keep', 'keeps', 'kind', 'knew', 'know', 'known', 'knows',
+'l', 'large', 'largely', 'last', 'later', 'latest', 'least',
+'less', 'let', 'lets', 'like', 'likely', 'long', 'longer',
+'longest', 'm', 'made', 'make', 'making', 'man', 'many',
+'may', 'me', 'member', 'members', 'men', 'might', 'more',
+'most', 'mostly', 'mr', 'mrs', 'much', 'must', 'my',
+'myself', 'n', 'necessary', 'need', 'needed', 'needing',
+'needs', 'never', 'new', 'newer', 'newest', 'next', 'no',
+'nobody', 'non', 'noone', 'not', 'nothing', 'now', 'nowhere',
+'number', 'numbers', 'o', 'of', 'off', 'often', 'old', 'older',
+'oldest', 'on', 'once', 'one', 'only', 'open', 'opened', 'opening',
+'opens', 'or', 'order', 'ordered', 'ordering', 'orders', 'other',
+'others', 'our', 'out', 'over', 'p', 'part', 'parted', 'parting',
+'parts', 'per', 'perhaps', 'place', 'places', 'point', 'pointed',
+'pointing', 'points', 'possible', 'present', 'presented', 'presenting',
+'presents', 'problem', 'problems', 'put', 'puts', 'q', 'quite', 'r',
+'rather', 'really', 'right', 'room', 'rooms', 's', 'said', 'same', 'saw',
+'say', 'says', 'second', 'seconds', 'see', 'seem', 'seemed', 'seeming',
+'seems', 'sees', 'several', 'shall', 'she', 'should', 'show', 'showed',
+'showing', 'shows', 'side', 'sides', 'since', 'small', 'smaller', 'smallest',
+'so', 'some', 'somebody', 'someone', 'something', 'somewhere', 'state', 'states',
+'still', 'such', 'sure', 't', 'take', 'taken', 'than', 'that', 'the', 'their',
+'them', 'then', 'there', 'therefore', 'these', 'they', 'thing', 'things',
+'think', 'thinks', 'this', 'those', 'though', 'thought', 'thoughts', 'three',
+'through', 'thus', 'to', 'today', 'together', 'too', 'took', 'toward',
+'turn', 'turned', 'turning', 'turns', 'two', 'u', 'under', 'until', 'up',
+'upon', 'us', 'use', 'used', 'uses', 'v', 'very', 'w', 'want', 'wanted',
+'wanting', 'wants', 'was', 'way', 'ways', 'we', 'well', 'wells', 'went',
+'were', 'what', 'when', 'where', 'whether', 'which', 'while', 'who', 'whole',
+'whose', 'why', 'will', 'with', 'within', 'without', 'work', 'worked', 'working',
+'works', 'would', 'x', 'y', 'year', 'years', 'yet', 'you', 'young', 'younger',
+'youngest', 'your', 'yours', 'z'})
 
 def preprocess(review):
     """
@@ -49,7 +87,7 @@ def preprocess(review):
     # Remove stop words.
     processed_review = [it for it in processed_review if it not in stop_words]
 
-    return processed_review
+    return processed_review[-MAX_WORDS_IN_REVIEW:-1]
 
 def define_graph():
     """
@@ -69,7 +107,7 @@ def define_graph():
 
     # Nick: To confirm: what should the shape of the input and output tensors be?
     learning_rate = 0.001 # Nick: it feels weird putting this logic in here.
-    num_units = 7 # Nick: Also arbitrary, unsure if more units = better.
+    num_units = 32 # Nick: Also arbitrary, unsure if more units = better.
     input_data = tf.placeholder(tf.float32,
                                 [BATCH_SIZE, MAX_WORDS_IN_REVIEW, EMBEDDING_SIZE],
                                 "input_data")
@@ -86,7 +124,7 @@ def define_graph():
     ###
     rnn_layers = \
     [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(num_units),
-                                   output_keep_prob=dropout_keep_prob) for x in range(3)]
+                                   output_keep_prob=dropout_keep_prob) for x in range(2)]
     multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
     rnn_output, state = tf.nn.dynamic_rnn(cell=multi_rnn_cell, inputs=input_data, dtype=tf.float32)
     ###
